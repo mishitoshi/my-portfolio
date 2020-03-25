03cc9a12c552835c77fb362bc8b5f841fb0c0bda
mishitoshi <toshimishito000@gmail.com>
2020-03-26 01:37:35 +0900

    change: headerデザイン

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list :two-line="twoLine">
+        <v-subheader>Sitemap</v-subheader>
+        <v-list-item-group color="primary" v-model="item">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-html="item.title"></v-list-item-title>
+              <v-list-item-subtitle v-html="item.content"></v-list-item-subtitle
+            ></v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app color="primary" dark absolut>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({
+    drawer: null,
+    twoLine: true,
+    item: 4,
+    items: [
+      {
+        title: "Profile",
+        content: "プロフィールです。"
+      },
+      {
+        title: "skills",
+        content: "使用可能なスキルです。"
+      },
+      {
+        title: "carriers",
+        content: "今までの経歴です。"
+      },
+      {
+        title: "link",
+        content: "私に関わるリンクです。"
+      }
+    ]
+  })
+};
+</script>
+
+<style scoped lang="scss"></style>
