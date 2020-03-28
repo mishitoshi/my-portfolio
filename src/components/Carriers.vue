06be32dfc43a9c2bfbe85d313d381fe2d7306297
mishitoshi <toshimishito000@gmail.com>
2020-03-29 03:04:00 +0900

    add: 形だけ完成

--- src/components/Carriers.vue
+++ src/components/Carriers.vue
@@ -1,11 +1,67 @@
 <template>
-  <div></div>
+  <div>
+    <Title :title="title" />
+    <v-timeline>
+      <v-timeline-item
+        v-for="(carrier, i) in carriers"
+        :key="i"
+        small
+        :color="color"
+      >
+        <template v-slot:opposite>
+          <span v-text="carrier.year"></span>
+        </template>
+        <div>
+          <h2>{{ carrier.title }}</h2>
+          <div>
+            {{ carrier.text }}
+          </div>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </div>
 </template>
 
 <script>
+import Title from "./Title";
+
 export default {
-  name: "Carriers"
+  name: "Carriers",
+  components: {
+    Title
+  },
+  data: () => ({
+    title: "Carriers",
+    carriers: [
+      {
+        title: "明治大学入学",
+        text: "明治大学に入学しました。",
+        year: "2016/04"
+      },
+      {
+        title: "明治大学入学",
+        text: "明治大学に入学しました。",
+        year: "2016/04"
+      },
+      {
+        title: "明治大学入学",
+        text: "明治大学に入学しました。",
+        year: "2016/04"
+      },
+      {
+        title: "明治大学入学",
+        text: "明治大学に入学しました。",
+        year: "2016/04"
+      },
+      {
+        title: "明治大学入学",
+        text: "明治大学に入学しました。",
+        year: "2016/04"
+      }
+    ],
+    color: "amber lighten-2"
+  })
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
