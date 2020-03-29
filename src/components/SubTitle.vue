2c6373e627488130d38d6f0696308d40284b759d
mishitoshi <toshimishito000@gmail.com>
2020-03-30 02:20:13 +0900

    add: Skillsページ

---
+++ src/components/SubTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>{{ subTitle }}</h3>
+</template>
+
+<script>
+export default {
+  name: "SubTitle",
+  props: {
+    subTitle: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped></style>
