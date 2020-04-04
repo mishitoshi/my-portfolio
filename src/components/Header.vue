8dbcf165ec4e8014a6c60acfba61cab2f0dcaf87
mishitoshi <toshimishito000@gmail.com>
2020-04-05 00:58:43 +0900

    change: color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,7 @@
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar app color="amber lighten-4" dark hide-on-scroll>
+    <v-app-bar app color="white" hide-on-scroll>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
   </header>
